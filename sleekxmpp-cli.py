56022828067bd73a5658bcfd6615f424757a1a55
cyfrost <cyrus.frost@hotmail.com>
2020-02-15 15:51:06 +0530

    allow immediate exit after xmpp SEND op

--- sleekxmpp-cli.py
+++ sleekxmpp-cli.py
@@ -8,14 +8,17 @@ class EchoBot(sleekxmpp.ClientXMPP):
     to_jid = None
     message_to_send = None
     is_verbose_logging_enabled = None
+    should_exit_after_send = None
 
-    def __init__(self, jid, pwd, to_jid, message, is_verbose_logging_enabled):
+    def __init__(self, jid, pwd, to_jid, message, should_exit_after_send, is_verbose_logging_enabled):
         sleekxmpp.ClientXMPP.__init__(self, jid, pwd)
         self.to_jid = to_jid
         self.message_to_send = message
         self.is_verbose_logging_enabled = is_verbose_logging_enabled
+        self.should_exit_after_send = should_exit_after_send
         self.add_event_handler('session_start', self.start)
-        self.add_event_handler('message', self.message)
+        if not self.should_exit_after_send:
+            self.add_event_handler('message', self.message)
 
     def start(self, event):
         self.send_presence()
@@ -23,6 +26,8 @@ class EchoBot(sleekxmpp.ClientXMPP):
         msg = str.format("Sending message: \"{}\" to user \"{}\"", self.message_to_send, self.to_jid)
         self.logToStdout(is_verbose_logging_enabled, msg)
         self.send_message(self.to_jid, self.message_to_send)
+        if self.should_exit_after_send:
+            sys.exit(0)
         self.logToStdout(is_verbose_logging_enabled, "Message sent! awaiting response...")
 
     def logToStdout(self, is_logging_enabled, msg):
@@ -45,6 +50,7 @@ if __name__ == "__main__":
     optp.add_option('-p', '--password', dest='password', help="From JID password")
     optp.add_option('-t', '--to-jid', dest="to_jid", help="To JID")
     optp.add_option('-m', '--message', dest="send_message", help="Message to send")
+    optp.add_option('-n', '--no-wait', dest="no_await_response", help="Send message and exit immediately; no wait for response", action='store_const', const='no_await_response')
     optp.add_option('-v', '--verbose', dest="verbose", help="Enable verbose logging", action='store_const', const='verbose')
 
     opts, args = optp.parse_args()
@@ -54,6 +60,7 @@ if __name__ == "__main__":
     from_jid_pass = opts.password
     to_jid = opts.to_jid
     message_to_send = opts.send_message
+    should_exit_after_send = opts.no_await_response is not None
 
     if from_jid is None or from_jid_pass is None or to_jid is None or message_to_send is None:
         print("\nInsufficient arguments!\n")
@@ -61,7 +68,7 @@ if __name__ == "__main__":
 
     logToStdout(is_verbose_logging_enabled, "Info: Settings OK. Connecting to XMPP server...")
 
-    xmpp = EchoBot(from_jid, from_jid_pass, to_jid, message_to_send, is_verbose_logging_enabled)
+    xmpp = EchoBot(from_jid, from_jid_pass, to_jid, message_to_send, should_exit_after_send, is_verbose_logging_enabled)
     xmpp.register_plugin('xep_0030')  # service discovery
     xmpp.register_plugin('xep_0004')  # date form
     xmpp.register_plugin('xep_0060')  # pubsub
